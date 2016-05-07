d6c7f35d668f2513e6b4b083a126ad5283c4580f
yixi1992 <yixi1992@gmail.com>
2016-05-07 18:05:30 -0400

    update

--- f1b1/train.sh
+++ f1b1/train.sh
@@ -17,4 +17,4 @@ cd ~/work/yixi/segnet/segnetf1/
 
 #~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/segnet/f1b1/segnet_basic_solver.prototxt -weights ~/segnet/f1b1/trainedrgbbs10_surg.caffemodel
 
-~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/segnet/f1b1/segnet_basic_solver.prototxt -snapshot /home-4/yixi@umd.edu/segnet/f1b1/snapshots/f1b1trgbbs10lr1e-3fixed_iter_12000.solverstate
+~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/segnet/f1b1/segnet_basic_solver.prototxt -snapshot /home-4/yixi@umd.edu/segnet/f1b1/snapshots/f1b1trgbslicelr1e-3fixed_iter_15000.solverstate
--- train.sh
+++ train.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#SBATCH -t 10:00:00
+#SBATCH -t 20:00:00
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --mail-user=yixi@cs.umd.edu
-#SBATCH --mail-type=BEGIN
+##SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
-#SBATCH --job-name="segnetf1"
+#SBATCH --job-name="rgb"
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 #SBATCH --mem=5000
 
 cd ~/work/yixi/segnet/segnetf1/
-~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/work/yixi/segnet/segnetf1/segnet_basic_solver.prototxt -snapshot /home-4/yixi@umd.edu/segnet/snapshots/trgbbs10learngglr1e-3fixed_iter_10000.solverstate
+#~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/work/yixi/segnet/segnetf1/segnet_basic_solver.prototxt -snapshot /home-4/yixi@umd.edu/segnet/snapshots/trgbbs10learngglr1e-3fixed_iter_10000.solverstate
 #~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/work/yixi/segnet/segnetf1/segnet_basic_solver.prototxt -snapshot /home-4/yixi@umd.edu/segnet/snapshots/trgbbs10learngglr1e-4fixed_iter_3000.solverstate
 
 
@@ -26,4 +26,5 @@ cd ~/work/yixi/segnet/segnetf1/
 
 #~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/work/yixi/segnet/segnetf1/segnet_basic_solver.prototxt -snapshot /home-4/yixi@umd.edu/segnet/snapshots/scratchgglr1e-1fixed_iter_12100.solverstate
 
+~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/segnet/segnet_basic_solver.prototxt
 #~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver ~/segnet/segnet_basic_solver.prototxt -weights ~/work/yixi/segnet/segnetf1/basic_camvid_surg.caffemodel
